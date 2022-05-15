75cbb632ee94bcf7bf91739d4e9e74160a7b658b
Jessie Fehrenbach <99111335+jessie5555@users.noreply.github.com>
2022-05-15 01:08:48 -0600

    Changes to data collection
    I dont know why but my bash isnt really working so thats why I just used github for this commit. I might fix it, might not. who knows

--- optiondatacollection.py
+++ optiondatacollection.py
@@ -1,13 +1,58 @@
-from cmath import exp
 import robin_stocks.robinhood as rs
 import os
-robin_usr = os.environ.get("robinhood_username")
-robin_pass = os.environ.get("robinhood_password")
-rs.login(username=robin_usr,
-        password=robin_pass,
-        expiresIn=120,
-        by_sms=True
-)
+import yfinance as yf
+import pandas
 
-tsla = rs.options.get_option_historicals('TSLA','2022-05-13' , '800','call' ,interval='hour',span='week', bounds='regular')
-print(tsla)
+
+class optiondatacollection:
+    def __init__(self):
+        self.robin_usr = os.environ.get("robinhood_username")
+        self.robin_pass = os.environ.get("robinhood_password")
+        rs.login(username=self.robin_usr,
+                password=self.robin_pass,
+                expiresIn=500,
+            by_sms=True
+        )
+    #def tickers(self):
+
+    def time(year, month, day):
+        if month < 10:
+            month = '0' + str(month)
+        if day < 10:
+            day = '0' + str(day)
+
+        string = str(year) + '-' + str(month) + '-' + str(day)
+        return string
+
+    def historicals(self,ticker, per):
+        tickerInfo = yf.Ticker(ticker)
+        self.data = tickerInfo.history(period=per)
+        
+    def option(self, ticker):
+        print(self.data)
+        # alright so this enumerations kinda weird, I dont want to fix it. But,
+        # so the row is a tuple with two things in it, the first is the timestamp
+        # and the second is literally THE REST OF THE DATA
+        # So you need to iterate through the second tuple as a 2d array. Noted...
+        for id, row  in enumerate(self.data.iterrows()):
+            
+            if id == 0:
+                pass
+            date = str(row[0]).split(' ')
+            date = date[0]
+            strike = (row[1][0] + row[1][3]) / 2   # this was just a monkey brain way of finding a strike price 
+            
+            
+            try: 
+                spy = rs.options.get_option_historicals(ticker, date, strike,'call', interval='hour',span='week', bounds='regular')
+                print(spy)
+
+            except:
+                pass
+
+#
+
+
+rob = optiondatacollection()
+rob.historicals('SPY', '6mo')
+rob.option('SPY')
