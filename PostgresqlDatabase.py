8d860cbde730abec60488900f0c80d95f06542de
Jessie Fehrenbach <99111335+jessie5555@users.noreply.github.com>
2022-05-17 21:34:09 -0600

    Made the database more automated
    This dont work yet but im getting there.

--- PostgresqlDatabase.py
+++ PostgresqlDatabase.py
@@ -1,48 +1,49 @@
 import psycopg2
+import csv
+
+
 class PostgresqlDatabase():
     def __init__(self):
         self.conn =psycopg2.connect(
-    database = "tickers",
-    user='jessie',
-    password='********',
-    host='localhost',
-    port='5432'
+        database = "tickers",
+        user='jessie',
+        password='password',
+        host='localhost',
+        port='5432'
 )
         self.conn.autocommit = True
         self.cursor  = self.conn.cursor()
         
-    
-    def create_tables(self):
-        commands = (
-            """
-            CREATE TABLE ticker (
-                ticker_id SERIAL PRIMARY KEY,
-                ticker_name VARCHAR(255) NOT NULL
-            )
+    def create_stock_tables(self):
+        path = "./tickers.csv"
+        f = open(path, "r")
+        cols = []
+        while (True):
+            first = f.readline().strip()
+            if not first:
+                break
+            cols.append(first)
+        print(cols)
+        for col in cols:
+            print(col)
+            sqlQuery = 'DROP TABLE IF EXISTS ' + col +  ";\n"
+            sqlQuery += 'CREATE TABLE ' + col + ' (' + '\n'
+            sqlQuery += 'id serial PRIMARY KEY, \n'
+            sqlQuery += 'date DATE NOT NULL, \n' 
+            sqlQuery += 'time TIME NOT NULL, \n'
+            sqlQuery += "open_price REAL NOT NULL,\n"
+            sqlQuery += "close_price REAL NOT NULL \n"
 
-            CREATE TABLE experation (
-                id = SERIAL PRIMARY KEY
-                ticker = VARCHAR255 NOT NULL
-                expdata = DATE 
-                CONSTRAINT fk_ticker FORIEGN KEY(ticker) REFERENCES ticker(ticker_name)
-
-
-            )
-            
-            
-            """
-
-
-
-
-
-        )
-
-
+            sqlQuery = sqlQuery[:-2]
+            sqlQuery += ");"
+        print(sqlQuery)
+        self.cursor.execute(sqlQuery)
+        self.conn.commit()
+        self.cursor.close()
 
 
 
+con = PostgresqlDatabase()
+con.create_stock_tables()
 
 
-
-conn.close
